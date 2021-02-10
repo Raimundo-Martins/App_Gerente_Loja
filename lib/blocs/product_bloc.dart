bd33bb2f63848581df27965243a032a9faac84b4
Raimundo-Martins <cosmomartinsj@gmail.com>
2021-02-10 15:21:18 -0300

    Atualizando

--- lib/blocs/product_bloc.dart
+++ lib/blocs/product_bloc.dart
@@ -1,14 +1,35 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/subjects.dart';
 
 class ProductBloc extends BlocBase {
   String categoryId;
   DocumentSnapshot product;
 
-  ProductBloc({this.categoryId, this.product}){
+  final _productController = BehaviorSubject<Map>();
+  Stream<Map> get outProduct => _productController.stream;
 
+  Map<String, dynamic> unsavedDado;
+
+  ProductBloc({this.categoryId, this.product}) {
+    if (product != null) {
+      unsavedDado = Map.of(product.data);
+      unsavedDado['images'] = List.of(product.data['images']);
+      unsavedDado['sizes'] = List.of(product.data['sizes']);
+    } else {
+      unsavedDado = {
+        'title': null,
+        'description': null,
+        'price': null,
+        'images': [],
+        'sizes': []
+      };
+    }
+    _productController.add(unsavedDado);
   }
 
   @override
-  void dispose() {}
+  void dispose() {
+    _productController.close();
+  }
 }
--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -2,17 +2,34 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:gerente_loja/blocs/product_bloc.dart';
 
-class ProductScreen extends StatelessWidget {
+class ProductScreen extends StatefulWidget {
   final String categoryId;
   final DocumentSnapshot product;
-  final ProductBloc _productBloc;
-  final _formKey = GlobalKey<FormState>();
 
-  ProductScreen({this.categoryId, this.product})
+  ProductScreen({this.categoryId, this.product});
+
+  @override
+  _ProductScreenState createState() => _ProductScreenState(categoryId, product);
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final ProductBloc _productBloc;
+
+  _ProductScreenState(String categoryId, DocumentSnapshot product)
       : _productBloc = ProductBloc(categoryId: categoryId, product: product);
 
   @override
   Widget build(BuildContext context) {
+    final _style = TextStyle(color: Colors.white, fontSize: 16);
+
+    InputDecoration _buildDecoration(String label) {
+      return InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.grey),
+      );
+    }
+
     return Scaffold(
       backgroundColor: Colors.grey[850],
       appBar: AppBar(
@@ -31,10 +48,40 @@ class ProductScreen extends StatelessWidget {
       ),
       body: Form(
         key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16),
-          children: [],
-        ),
+        child: StreamBuilder<Map>(
+            stream: _productBloc.outProduct,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return Container();
+              return ListView(
+                padding: EdgeInsets.all(16),
+                children: [
+                  TextFormField(
+                    initialValue: snapshot.data['title'],
+                    style: _style,
+                    decoration: _buildDecoration('Título'),
+                    onSaved: (valor) {},
+                    validator: (valor) {},
+                  ),
+                  TextFormField(
+                    initialValue: snapshot.data['description'],
+                    maxLines: 5,
+                    style: _style,
+                    decoration: _buildDecoration('Descrição'),
+                    onSaved: (valor) {},
+                    validator: (valor) {},
+                  ),
+                  TextFormField(
+                    initialValue: snapshot.data['price']?.toStringAsFixed(2),
+                    keyboardType:
+                        TextInputType.numberWithOptions(decimal: true),
+                    style: _style,
+                    decoration: _buildDecoration('Preço'),
+                    onSaved: (valor) {},
+                    validator: (valor) {},
+                  ),
+                ],
+              );
+            }),
       ),
     );
   }
